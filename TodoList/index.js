0eb0722587cb67e62a9bc772c11e165b6d89306f
show3567 <ydong011@fiu.edu>
2022-01-28 14:46:13 -0600

    updata

--- TodoList/index.js
+++ TodoList/index.js
@@ -1,13 +1,17 @@
 const Appapi = (() => {
-
-    baseurl = 'https://jsonplaceholder.typicode.com';
-    path = 'todos';
+    baseurl = "https://jsonplaceholder.typicode.com";
+    path = "todos";
 
     const getTodos = () =>
-        fetch([baseurl, path].join('/'))
-            .then((response) => response.json());
+        fetch([baseurl, path].join("/")).then((response) => response.json());
+
+    const deleteTodo = id =>
+        fetch([baseurl, path, id].join("/"), {
+            method: "DELETE",
+        });
 
     return {
+        deleteTodo,
         getTodos,
     };
 })();
@@ -15,53 +19,91 @@ const Appapi = (() => {
 // ~~~~~~~~~~~~~~~view~~~~~~~~~~~~~~~~~~~~~
 const View = (() => {
     const domstr = {
-        todolist: '#todolist__container'
-    }
+        todolist: "#todolist__container",
+        deletebtn: ".delete_btn",
+    };
     const render = (element, tmp) => {
         element.innerHTML = tmp;
-    }
-    const createTmp = arr => {
-        let tmp = '';
-        arr.forEach(ele => {
+    };
+    const createTmp = (arr) => {
+        let tmp = "";
+        arr.forEach((ele) => {
             tmp += `
                 <li>
                     <span>${ele.title}</span>
-                    <button>X</button>
+                    <button class="delete_btn" id="${ele.id}">
+                        X
+                    </button>
                 </li>
             `;
         });
         return tmp;
-    }
+    };
 
     return {
         domstr,
         render,
-        createTmp
+        createTmp,
     };
 })();
 
 // ~~~~~~~~~~~~~~~model~~~~~~~~~~~~~~~~~~~~~
-const Model = ((api) => {
+const Model = ((api, view) => {
+    class Todo {
+        constructor(userId, id, title, completed) {
+            this.userId = userId;
+            this.id = id;
+            this.title = title;
+            this.completed = completed;
+        }
+    }
 
+    class State {
+        #todolist = [];
 
+        get todolist() {
+            return this.#todolist;
+        }
+
+        set todolist(newdata) {
+            this.#todolist = newdata;
+
+            const ele = document.querySelector(view.domstr.todolist);
+            const tmp = view.createTmp(this.#todolist);
+            view.render(ele, tmp);
+
+            const deletebtns = document.querySelectorAll(view.domstr.deletebtn);
+            deletebtns.forEach((btn) => {
+                btn.addEventListener("click", (event) => {
+                    this.todolist = this.#todolist.filter(
+                        (todo) => +todo.id !== +event.target.id
+                    );
+                    api.deleteTodo(id);
+                });
+            });
+        }
+    }
+
+    const deleteTodo = api.deleteTodo;
     const getTodos = api.getTodos;
 
     return {
-        getTodos
+        Todo,
+        State,
+        getTodos,
+        deleteTodo
     };
-})(Appapi);
+})(Appapi, View);
+
 // ~~~~~~~~~~~~~~~controller~~~~~~~~~~~~~~~~~~~~~
 const Controller = ((model, view) => {
+    const state = new model.State();
 
     const init = () => {
-        model.getTodos().then(data => {
-            const ele = document.querySelector(view.domstr.todolist);
-            const tmp = view.createTmp(data);
-
-            view.render(ele, tmp);
+        model.getTodos().then((data) => {
+            state.todolist = data;
         });
-    }
-
+    };
 
     return { init };
 })(Model, View);
