b29b020f23ed43d4b6abdbfbbcf5dd906263060c
show3567 <ydong011@fiu.edu>
2022-01-28 07:55:27 -0600

    update

--- Javascript/index.js
+++ Javascript/index.js
@@ -15,102 +15,107 @@
 
 // console.log(runAll(1)(callback1, callback2, callback3)); // 4
 
-// class MyPromise {
-//     thenfnqueue = [];
-//     catchfnqueue = [];
-//     currentdata = undefined;
-//     promiseState = "pending";
+class MyPromise {
+    thenfnqueue = [];
+    catchfnqueue = [];
+    currentdata = undefined;
+    promiseState = "pending";
 
-//     constructor(executor) {
-//         // console.log('constructor: ', this); // <-------
-//         try {
-//             executor(this.resolve.bind(this), this.reject);
-//         } catch (error) {
-//             this.reject(error);
-//         }
-//     }
-//     resolve(resdata) {
-//         setTimeout(() => {
-//             try {
-//                 if (this.promiseState === "pending")
-//                     this.promiseState = "fulfilled";
+    constructor(executor) {
+        // console.log('constructor: ', this); // <-------
+        try {
+            executor(this.resolve.bind(this), this.reject);
+        } catch (error) {
+            this.reject(error);
+        }
+    }
+    resolve(resdata) {
+        if (this.promiseState === "pending") {
+            this.promiseState = "fulfilled";
 
-//                 this.currentdata = resdata;
+            setTimeout(() => {
+                try {
+                    this.currentdata = resdata;
 
-//                 while (this.thenfnqueue.length) {
-//                     const cb = this.thenfnqueue.shift();
+                    while (this.thenfnqueue.length) {
+                        const cb = this.thenfnqueue.shift();
 
-//                     if (this.currentdata instanceof MyPromise) {
-//                         this.currentdata.then((data) => {
-//                             this.currentdata = cb(data);
-//                         });
-//                     } else {
-//                         this.currentdata = cb(this.currentdata);
-//                     }
-//                 }
-//             } catch (error) {
-//                 this.reject(error);
-//             }
-//         }, 0);
-//     }
-//     reject = (rejdata) => {
-//         this.promiseState = "rejected";
-//         setTimeout(() => {
-//             const cb = this.catchfnqueue.shift();
-//             if (cb) {
-//                 cb(rejdata);
-//             } else {
-//                 console.log(rejdata);
-//             }
-//         }, 0);
-//     };
-//     then(thenfn) {
-//         if (this.promiseState === "pending") {
-//             this.thenfnqueue.push(thenfn);
-//         }
-//         return this;
-//     }
-//     catch(catchfn) {
-//         this.catchfnqueue.push(catchfn);
-//         return this;
-//     }
+                        if (this.currentdata instanceof MyPromise) {
+                            this.currentdata.then((data) => {
+                                this.currentdata = cb(data);
+                            });
+                        } else {
+                            this.currentdata = cb(this.currentdata);
+                        }
+                    }
+                } catch (error) {
+                    this.reject(error);
+                }
+            }, 0);
+        } else return;
+    }
+    reject = (rejdata) => {
+        if (this.promiseState === "pending") {
+            this.promiseState = "rejected";
+            setTimeout(() => {
+                const cb = this.catchfnqueue.shift();
+                if (cb) {
+                    cb(rejdata);
+                } else {
+                    console.log(rejdata);
+                }
+            }, 0);
+        } else return;
+    };
+    then(thenfn) {
+        if (this.promiseState === "fulfilled") {
+            this.thenfnqueue.push(thenfn);
+        }
+        return this;
+    }
+    catch(catchfn) {
+        if (this.promiseState === "rejected") {
+            this.catchfnqueue.push(catchfn);
+        }
+        return this;
+    }
 
-//     static resolve(resdata) {
-//         return new MyPromise((res, _) => {
-//             res(resdata);
-//         });
-//     }
-//     static reject(rejdata) {
-//         return new MyPromise((_, rej) => {
-//             rej(rejdata);
-//         });
-//     }
-//     static all(arr) {
-//         // console.log(arr);
-//         const resolveArr = new Array(arr.length);
-//         let counter = 0;
+    static resolve(resdata) {
+        return new MyPromise((res, _) => {
+            res(resdata);
+        });
+    }
+    static reject(rejdata) {
+        return new MyPromise((_, rej) => {
+            rej(rejdata);
+        });
+    }
+    static all(arr) {
+        // console.log(arr);
+        const resolveArr = new Array(arr.length);
+        let counter = 0;
 
-//         return new MyPromise((res, rej) => {
-//             arr.forEach((promise, i) => {
-//                 if (promise instanceof MyPromise) {
-//                     promise.then((resData) => {
-//                         counter++;
-//                         resolveArr[i] = resData;
-//                         if (counter === resolveArr.length) {
-//                             res(resolveArr);
-//                         }
-//                     });
-//                 } else {
-//                     counter++;
-//                     resolveArr[i] = promise;
-//                     if (counter === resolveArr.length) {
-//                         res(resolveArr);
-//                     }
-//                 }
-//             });
-//         });
-//     }
-// }
+        return new MyPromise((res, rej) => {
+            arr.forEach((promise, i) => {
+                if (promise instanceof MyPromise) {
+                    promise.then((resData) => {
+                        counter++;
+                        resolveArr[i] = resData;
+                        if (counter === resolveArr.length) {
+                            res(resolveArr);
+                        }
+                    });
+                } else {
+                    counter++;
+                    resolveArr[i] = promise;
+                    if (counter === resolveArr.length) {
+                        res(resolveArr);
+                    }
+                }
+            });
+        });
+    }
+}
 
 // const promise1 = MyPromise.resolve(3);
 
@@ -125,22 +130,22 @@
 
 // expected output: Array [3, 42, "foo"]
 
-// const getRendomTime = () => Math.floor(Math.random() * 6);
+const getRendomTime = () => Math.floor(Math.random() * 6);
 
-// new MyPromise((resolve, reject) => {
-//     const timer = getRendomTime();
-//     console.log(`wait for ${timer}s`);
-//     console.log(a);
-//     if (timer > 3) {
-//         resolve("resolve");
-//     } else {
-//         reject("reject");
-//     }
-
-//     // reject("resolve");
-// })
-//     .then(console.log)
-//     .catch(console.log);
+new MyPromise((resolve, reject) => {
+    const timer = getRendomTime();
+    console.log(`wait for ${timer}s`);
+    // console.log(a);
+    if (timer > 3) {
+        resolve("resolve");
+    } else {
+        reject("reject");
+    }
+    // reject("reject");
+    // resolve("resolve");
+})
+    // .then(console.log)
+    // .catch(console.log);
 
 // const p = new Promise((res, rej) => {});
 // console.log(p);
@@ -190,36 +195,36 @@
 //     }
 // })();
 
-function myFetch(url, option) {
-    const method = option && option.method ? option.method : "GET";
+// function myFetch(url, option) {
+//     const method = option && option.method ? option.method : "GET";
 
-    return new Promise((resolve, reject) => {
-        const xhttp = new XMLHttpRequest();
-        xhttp.open(method, url, true);
+//     return new Promise((resolve, reject) => {
+//         const xhttp = new XMLHttpRequest();
+//         xhttp.open(method, url, true);
 
-        if (option && option.headers) {
-            Object.keys(option.headers).forEach((key) => {
-                xhttp.setRequestHeader(key, option.headers[key]);
-            });
-        }
+//         if (option && option.headers) {
+//             Object.keys(option.headers).forEach((key) => {
+//                 xhttp.setRequestHeader(key, option.headers[key]);
+//             });
+//         }
 
-        xhttp.onreadystatechange = function () {
-            if (
-                this.readyState == 4 &&
-                this.status >= 200 &&
-                this.status < 300
-            ) {
-                console.log(this.status);
-                resolve({
-                    json: function () {
-                        return JSON.parse(xhttp.response);
-                    },
-                });
-            }
-        };
-        option && option.body ? xhttp.send(option.body) : xhttp.send();
-    });
-}
+//         xhttp.onreadystatechange = function () {
+//             if (
+//                 this.readyState == 4 &&
+//                 this.status >= 200 &&
+//                 this.status < 300
+//             ) {
+//                 console.log(this.status);
+//                 resolve({
+//                     json: function () {
+//                         return JSON.parse(xhttp.response);
+//                     },
+//                 });
+//             }
+//         };
+//         option && option.body ? xhttp.send(option.body) : xhttp.send();
+//     });
+// }
 
 // myFetch("https://jsonplaceholder.typicode.com/todos/1")
 //     .then((response) => response.json())
@@ -239,17 +244,17 @@ function myFetch(url, option) {
 //     .then((response) => response.json())
 //     .then((json) => console.log(json));
 
-myFetch("https://jsonplaceholder.typicode.com/posts/1", {
-    method: "PUT",
-    body: JSON.stringify({
-        id: 1,
-        title: "foo",
-        body: "bar",
-        userId: 1,
-    }),
-    headers: {
-        "Content-type": "application/json; charset=UTF-8",
-    },
-})
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+// myFetch("https://jsonplaceholder.typicode.com/posts/1", {
+//     method: "PUT",
+//     body: JSON.stringify({
+//         id: 1,
+//         title: "foo",
+//         body: "bar",
+//         userId: 1,
+//     }),
+//     headers: {
+//         "Content-type": "application/json; charset=UTF-8",
+//     },
+// })
+//     .then((response) => response.json())
+//     .then((json) => console.log(json));
