661e0bb2681be451c9f1437ac4a9680fde9a36f4
Patrick-FRE <patrick.lin@antra.com>
2022-02-08 13:19:48 -0500

    update

---
+++ HTML/index.js
@@ -0,0 +1,9 @@
+// let element = document.querySelector('body');
+// console.dir(element);
+
+/// Javascript = ES + Web API
+/// Ecmascript : ES5 ES6 ES7 | Syntax standard
+/// NodeJS = ES + Node API
+
+const fs = require('fs');
+console.log(fs);
