3ee48add5cfd590f73d917e3a0ed3a49f2711605
Yicheng Ye <ye.yicheng123@gmail.com>
2022-02-11 16:36:25 -0500

    refactor

--- React/02_EventListApp/src/App.js
+++ React/02_EventListApp/src/App.js
@@ -4,132 +4,139 @@ import {Api} from "./API/api";
 import EventListRow from "./Component/EventListRow";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      ...props,
-      eventList: []
+    constructor(props) {
+        super(props);
+        this.state = {
+            ...props,
+            eventList: []
+        }
+        this.generateEventList = this.generateEventList.bind(this);
+        this.changeInput = this.changeInput.bind(this);
+        this.editEventRow = this.editEventRow.bind(this);
+        this.deleteEventRow = this.deleteEventRow.bind(this);
+        this.addEventRow = this.addEventRow.bind(this);
+        this.updateUI = this.updateUI.bind(this);
+        this.saveEventRow = this.saveEventRow.bind(this);
+        this.updateEventRow = this.updateEventRow.bind(this);
     }
-    this.generateEventList = this.generateEventList.bind(this);
-    this.changeInput = this.changeInput.bind(this);
-    this.editEventRow = this.editEventRow.bind(this);
-    this.deleteEventRow = this.deleteEventRow.bind(this);
-    this.addEventRow = this.addEventRow.bind(this);
-    this.updateUI = this.updateUI.bind(this);
-    this.saveEventRow = this.saveEventRow.bind(this);
-    this.updateEventRow = this.updateEventRow.bind(this);
-  }
 
-  async componentDidMount() {
-    const eventList = await Api.getEvents();
-    eventList.forEach(ev => {
-      ev.disabled = true
-    })
-    this.setState({
-      eventList: eventList
-    });
-  }
-
-  changeInput(e, id, keyName, valueType) {
-    const curr = this.state.eventList.filter(ev => ev.id === id)[0];
-    if (valueType === 'Date') {
-      curr[keyName] = new Date(e.target.value).getTime().toString()
-    } else {
-      curr[keyName] = e.target.value;
+    async componentDidMount() {
+        const eventList = await Api.getEvents();
+        eventList.forEach(ev => {
+            ev.disabled = true
+        })
+        this.setState({
+            eventList: eventList
+        });
     }
-    this.setState({
-      eventList: this.state.eventList
-    })
-  }
 
-  generateEventList() {
-    return this.state.eventList.map(ev => {
-      return <EventListRow ev={ev} key={ev.id}
-                  changeInput={this.changeInput}
-                  editEventRow={this.editEventRow}
-                  deleteEventRow={this.deleteEventRow}
-                  saveEventRow={this.saveEventRow}
-                  updateEventRow={this.updateEventRow}/>
-    })
-  }
-
-  async saveEventRow(e, id, ev) {
-    ev.isNew = undefined;
-    ev.id = undefined;
-    ev.disabled = true;
-    const newEv = await Api.addEvent(ev);
-    this.updateUI(e, id, newEv);
-  }
-
-  removeNewRow(e, id) {
-    this.setState({
-      eventList: this.state.eventList.filter(ev => ev.id !== id)
-    });
-  }
-
-  updateUI(e, id, newEv) {
-    let index = -1;
-    for (let i = 0; i < this.state.eventList.length; i++) {
-      if (this.state.eventList[i].id === id) {
-        index = i;
-      }
+    changeInput = (e, id, keyName, valueType) => {
+        const curr = this.state.eventList.filter(ev => ev.id === id)[0];
+        if (valueType === 'Date') {
+            curr[keyName] = new Date(e.target.value).getTime().toString()
+        } else {
+            curr[keyName] = e.target.value;
+        }
+        this.setState({
+            eventList: this.state.eventList
+        })
     }
-    this.state.eventList.splice(index, 1, newEv);
-    this.setState({
-      eventList: this.state.eventList
-    })
-  }
 
-  async updateEventRow(e, id, ev) {
-    ev.disabled = true;
-    const newEv = await Api.updateEvent(id, ev);
-    this.updateUI(e, id, newEv);
-  }
+    generateEventList = () => {
+        return this.state.eventList.map(ev => {
+            return <EventListRow ev={ev} key={ev.id}
+                                 changeInput={this.changeInput}
+                                 editEventRow={this.editEventRow}
+                                 deleteEventRow={this.deleteEventRow}
+                                 saveEventRow={this.saveEventRow}
+                                 updateEventRow={this.updateEventRow}/>
+        })
+    }
 
-  addEventRow() {
-    this.state.eventList.push({id: new Date().getTime().toString(), isNew: true, disabled: false, startDate: '', endDate: '', eventName: ''})
-    this.setState({
-      eventList: this.state.eventList
-    })
-  }
+    saveEventRow = async (e, id, ev) => {
+        ev.isNew = undefined;
+        ev.id = undefined;
+        ev.disabled = true;
+        const newEv = await Api.addEvent(ev);
+        this.updateUI(e, id, newEv);
+    }
 
-  async editEventRow(e, id) {
-    const curr = this.state.eventList.filter(ev => ev.id === id)[0];
-    curr.disabled = false;
-    this.setState({
-      eventList: this.state.eventList
-    })
-  }
+    removeNewRow = async (e, id) => {
+        this.setState({
+            eventList: this.state.eventList.filter(ev => ev.id !== id)
+        });
+    }
 
-  async deleteEventRow(e, id) {
-      await Api.deleteEvent(id);
-      this.setState({
-        eventList: this.state.eventList.filter(ev => ev.id !== id)
-      });
-  }
+    updateUI = (e, id, newEv) => {
+        let index = -1;
+        for (let i = 0; i < this.state.eventList.length; i++) {
+            if (this.state.eventList[i].id === id) {
+                index = i;
+            }
+        }
+        this.state.eventList.splice(index, 1, newEv);
+        this.setState({
+            eventList: this.state.eventList
+        })
+    }
 
-  render() {
-    return (
-        <div className="App">
-          <section className="table-container">
-            <div className="add-content">
-              <button onClick={this.addEventRow} className="add-btn">ADD NEW</button>
+    updateEventRow = async (e, id, ev) => {
+        ev.disabled = true;
+        const newEv = await Api.updateEvent(id, ev);
+        this.updateUI(e, id, newEv);
+    }
+
+    addEventRow = () => {
+        this.state.eventList.push({
+            id: new Date().getTime().toString(),
+            isNew: true,
+            disabled: false,
+            startDate: '',
+            endDate: '',
+            eventName: ''
+        })
+        this.setState({
+            eventList: this.state.eventList
+        })
+    }
+
+    editEventRow = async (e, id) => {
+        const curr = this.state.eventList.filter(ev => ev.id === id)[0];
+        curr.disabled = false;
+        this.setState({
+            eventList: this.state.eventList
+        })
+    }
+
+    deleteEventRow = async (e, id) => {
+        await Api.deleteEvent(id);
+        this.setState({
+            eventList: this.state.eventList.filter(ev => ev.id !== id)
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <section className="table-container">
+                    <div className="add-content">
+                        <button onClick={this.addEventRow} className="add-btn">ADD NEW</button>
+                    </div>
+                    <table>
+                        <tr className="header">
+                            <th>Event name</th>
+                            <th>Start date</th>
+                            <th>End date</th>
+                            <th>Actions</th>
+                        </tr>
+                        <tbody id="table-body">
+                        {this.generateEventList()}
+                        </tbody>
+                    </table>
+                </section>
             </div>
-            <table>
-              <tr className="header">
-                <th>Event name</th>
-                <th>Start date</th>
-                <th>End date</th>
-                <th>Actions</th>
-              </tr>
-              <tbody id="table-body">
-              {this.generateEventList()}
-              </tbody>
-            </table>
-          </section>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
