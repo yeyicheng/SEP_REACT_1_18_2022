de30efe8309ffafd7c82e92e6e9c88b7d8506680
Yicheng Ye <ye.yicheng123@gmail.com>
2022-02-25 19:14:25 -0500

    structure change

--- React/03_MySite/src/App.js
+++ React/03_MySite/src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
-import FirstScreen from "./Components/FirstScreen/FirstScreen";
-import SecondScreen from "./Components/SecondScreen/SecondScreen";
-import ThirdScreen from "./Components/ThirdScreen/ThirdScreen";
-import FourthScreen from "./Components/FourthScreen/FourthScreen";
-import FifthScreen from "./Components/FifthScreen/FifthScreen";
-import SixthScreen from "./Components/SixthScreen/SixthScreen";
+import Main from "./Components/FirstScreen/Main";
+import Info from "./Components/SecondScreen/Info";
+import Summary from "./Components/ThirdScreen/Summary";
+import Portfolio from "./Components/FourthScreen/Portfolio";
+import Contact from "./Components/FifthScreen/Contact";
+import Footer from "./Components/SixthScreen/Footer";
 
 function App() {
   return (
     <div className="App">
-      <FirstScreen/>
-        <SecondScreen/>
-        <ThirdScreen/>
-      <FourthScreen/>
-        <FifthScreen/>
-        <SixthScreen/>
+      <Main/>
+        <Info/>
+        <Summary/>
+      <Portfolio/>
+        <Contact/>
+        <Footer/>
     </div>
   );
 }
